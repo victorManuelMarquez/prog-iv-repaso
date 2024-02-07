b0123c880178d7ab663c6ae57a8080031ead0984
victor <victormmla3lm@gmail.com>
2024-02-07 12:20:40 -0300

    Ingreso de números por consola y acercamiento a las excepciones.

---
+++ IngresoDeDatos/src/IngresoDeDatos.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class IngresoDeDatos {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            boolean incorrecto;
+            int num1 = 0, num2 = 0;
+            do {
+                System.out.print("Ingrese un número: ");
+                try {
+                    num1 = sc.nextInt();
+                    incorrecto = false;
+                } catch (NumberFormatException formatException) {
+                    System.out.println("No ingresaste un número.");
+                    incorrecto = true;
+                }
+            } while (incorrecto);
+            do {
+                System.out.print("Ingrese un número: ");
+                try {
+                    num2 = sc.nextInt();
+                    incorrecto = false;
+                } catch (NumberFormatException formatException) {
+                    System.out.println("No ingresaste un número.");
+                    incorrecto = true;
+                }
+            } while (incorrecto);
+            System.out.println("Operaciones...");
+            System.out.printf("%d + %d = %d\n", num1, num2, num1+num2);
+            System.out.printf("%d - %d = %d\n", num1, num2, num1-num2);
+            System.out.printf("%d * %d = %d\n", num1, num2, num1*num2);
+            try {
+                System.out.printf("%d / %d = %.2f\n", num1, num2, (float) num1 / num2);
+                System.out.printf("%d div %d = %d resto %d\n", num1, num2, num1 / num2, num1 % num2);
+            } catch (ArithmeticException arithmeticException) {
+                System.out.println("Math ERROR!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+}
